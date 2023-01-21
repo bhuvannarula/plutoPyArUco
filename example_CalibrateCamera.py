046c025c8b45f7f306baf029931c5013a5ea2d38
Bhuvan Narula <55291162+bhuvannarula@users.noreply.github.com>
2023-01-21 06:12:51 +0530

    added camera calibration script

---
+++ example_CalibrateCamera.py
@@ -0,0 +1,19 @@
+from pluto_aruco import *
+from plutopy import plutoDrone
+
+drone = plutoDrone()
+
+aruco = plutoArUco(drone, arucoID=43)
+
+aruco.debug = True
+
+aruco.setOrigin()
+print(aruco.origin)
+
+aruco.start() # Starts the PID script
+
+# Press q to close camera feed
+
+# Now, wait until user presses 'Enter' key, then stop all code, and close the socket.
+_ = input()
+aruco.stop()
