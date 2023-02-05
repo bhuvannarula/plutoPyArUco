c403469b68ea0602c875b51428209d65aee7dbe5
lakshay162 <b21255@students.iitmandi.ac.in>
2023-02-06 00:28:53 +0530

    Create kalman_git.py

---
+++ kalman_git.py
@@ -0,0 +1,91 @@
+class KalmanFilter:
+    def __init__(self):
+        pass
+
+    def Configure(self, zVariance, zAccelVariance, zAccelBiasVariance, zInitial, vInitial, aBiasInitial):
+        self.zAccelVariance_ = zAccelVariance
+        self.zAccelBiasVariance_ = zAccelBiasVariance
+        self.zVariance_ = zVariance
+
+        self.z_ = zInitial
+        self.v_ = vInitial
+        self.aBias_ = aBiasInitial
+        self.Pzz_ = 1.0
+        self.Pzv_ = 0.0
+        self.Pza_ = 0.0
+
+        self.Pvz_ = 0.0
+        self.Pvv_ = 1.0
+        self.Pva_ = 0.0
+
+        self.Paz_ = 0.0
+        self.Pav_ = 0.0
+        self.Paa_ = 100000.0
+
+    def Update(self, z, a, dt, pZ, pV):
+        accel = a - self.aBias_
+        self.v_ += accel * dt
+        self.z_ += self.v_ * dt
+
+        self.zAccelVariance_ = abs(accel) / 50.0
+        self.zAccelVariance_ = min(self.zAccelVariance_, 50.0)
+        self.zAccelVariance_ = max(self.zAccelVariance_, 1.0)
+
+        dt2div2 = dt * dt / 2.0
+        dt3div2 = dt2div2 * dt
+        dt4div4 = dt2div2 * dt2div2
+
+        t00 = self.Pzz_ + dt * self.Pvz_ - dt2div2 * self.Paz_
+        t01 = self.Pzv_ + dt * self.Pvv_ - dt2div2 * self.Pav_
+        t02 = self.Pza_ + dt * self.Pva_ - dt2div2 * self.Paa_
+
+        t10 = self.Pvz_ - dt * self.Paz_
+        t11 = self.Pvv_ - dt * self.Pav_
+        t12 = self.Pva_ - dt * self.Paa_
+
+        t20 = self.Paz_
+        t21 = self.Pav_
+        t22 = self.Paa_
+
+        self.Pzz_ = t00 + dt * t01 - dt2div2 * t02
+        self.Pzv_ = t01 - dt * t02
+        self.Pza_ = t02
+
+        self.Pvz_ = t10 + dt * t11 - dt2div2 * t12
+        self.Pvv_ = t11 - dt * t12
+        self.Pva_ = t12
+
+        self.Paz_ = t20 + dt * t21 - dt2div2 * t22
+        self.Pav_ = t21 - dt * t22
+        self.Paa_ = t22
+
+        self.Pzz_ += dt4div4*self.zAccelVariance_
+        self.Pzv_ += dt3div2*self.zAccelVariance_
+
+        self.Pvz_ += dt3div2*self.zAccelVariance_
+        self.Pvv_ += dt*dt*self.zAccelVariance_
+
+        self.Paa_ += self.zAccelBiasVariance_
+
+        # Error
+        innov = z - self.z_
+        sInv = 1.0 / (self.Pzz_ + self.zVariance_)  
+
+        # Kalman gains
+        kz = self.Pzz_ * sInv
+        kv = self.Pvz_ * sInv
+        ka = self.Paz_ * sInv
+
+        # Update state 
+        self.z_ += kz * innov
+        self.v_ += kv * innov
+        self.aBias_ += ka * innov
+
+        # Update state covariance matrix
+        self.Paz_ -= ka * self.Pzz_
+        self.Pav_ -= ka * self.Pzv_
+        self.Paa_ -= ka * self.Pza_
+
+        self.Pzv_ -= kz * self.Pvz_
+        self.Pvv_ -= kz * self.Pvv_
+        self.Pva_ -= kz * self.Pva_
