c92f86e3fce1b555e0eae1c080c1f3abd71de29b
lakshay162 <b21255@students.iitmandi.ac.in>
2023-02-06 23:06:09 +0530

    grephhhh

---
+++ example_graph.py
@@ -0,0 +1,43 @@
+from pluto_aruco import *
+from plutoGraph import *
+from plutopy import plutoDrone
+
+# Creating a plutoDrone instance, which allows basic controls of drone, 
+# through MSP Communication
+drone = plutoDrone()
+
+# Set drone to throttle mode, as we will be using manual PID through ArUco Detection
+drone.control.throttleMode()
+#drone.control.altholdMode()
+
+# Starting MSP Communication with drone
+drone.start()
+
+# Creating an ArUco Control instance, that will perform PID on drone
+aruco = plutoArUco(drone, targetID = 85)
+# This should launch a camera feed.
+
+# Now, place the drone physically to a desired point, and set it as origin
+aruco.setOrigin()
+print(aruco.origin)
+
+# Now that origin is set, we can set TARGET for drone
+
+# So, for altitude hold, setting target:
+target = [
+    aruco.origin.X,
+    aruco.origin.Y,
+    20 # cm, Roughly 3.5 feet (3.5*30 = 105)
+]
+# Z (cm) should be height from ground (specifically, origin set initially)
+aruco.setTarget(*target)
+
+# Once initial target is set, we can take-off the drone, and start the PID
+drone.control.take_off()
+visual = graph(aruco)
+aruco.start() # Starts the PID script
+visual.animate()
+# Now, wait until user presses 'Enter' key, then stop all code, and close the socket.
+_ = input()
+drone.control.land()
+aruco.stop()
