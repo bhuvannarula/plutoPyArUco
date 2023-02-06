c2da6f556e2ec9217e06acd3c0a3506174317bcd
lakshay162 <b21255@students.iitmandi.ac.in>
2023-02-06 23:06:59 +0530

    comm

--- aruco/plutoPID.py
+++ aruco/plutoPID.py
@@ -64,36 +64,32 @@ class positionPID:
         vel_Z = state.V[Z]
 
         # Calculating the P-Term
-        result_X = constrain(self.pPOS[X] * pos_err[X], -500, 500)
-        result_Y = constrain(self.pPOS[Y] * pos_err[Y], -500, 500)
-        result_Z = constrain(self.pPOS[Z] * pos_err[Z], -200, 200)
         p_x = constrain(self.pPOS[X] * pos_err[X], -500, 500)
         p_y = constrain(self.pPOS[Y] * pos_err[Y], -500, 500)
         p_z = constrain(self.pPOS[Z] * pos_err[Z], -500, 500)
-
         # Calculating the I-Term
         self.iTerm[X] += (self.iPOS[X] * pos_err[X]) * dt / self.unit
         self.iTerm[Y] += (self.iPOS[Y] * pos_err[Y]) * dt / self.unit
         self.iTerm[Z] += (self.iPOS[Z] * pos_err[Z]) * dt / self.unit
         
+
         self.iTerm[X] = constrain(self.iTerm[X], -50, 50)
-        self.iTerm[Y] = constrain(self.iTerm[Y], -500, 500)
-        self.iTerm[Z] = constrain(self.iTerm[Z], -300, 300)
+        self.iTerm[Y] = constrain(self.iTerm[Y], -50, 50)
+        self.iTerm[Z] = constrain(self.iTerm[Z], -400, 400)
 
         result_X += self.iTerm[X]
         result_Y += self.iTerm[Y]
         result_Z += self.iTerm[Z]
-        i_x = self.iTerm[X]
-        i_y = self.iTerm[Y]
-        i_z = self.iTerm[Z]
-
+        i_x=self.iTerm[X]
+        i_y=self.iTerm[Y]
+        i_z=self.iTerm[Z]
         # Calculating the D-Term
-        result_X += constrain(self.dPOS[X] * (vel_X) * (-1), -1000, 1000)
-        result_Y += constrain(self.dPOS[Y] * (vel_Y) * (-1), -1000, 1000)
-        result_Z += constrain(self.dPOS[Z] * (vel_Z) * (-1), -300, 300)
+        result_X += constrain(self.dPOS[X] * (vel_X) * (-1), -500, 500)
+        result_Y += constrain(self.dPOS[Y] * (vel_Y) * (-1), -500, 500)
+        result_Z += constrain(self.dPOS[Z] * (vel_Z) * (-1), -500, 500)
         d_x= constrain(self.dPOS[X] * (vel_X) * (-1), -500, 500)
         d_y= constrain(self.dPOS[Y] * (vel_Y) * (-1), -500, 500)
-        d_z= constrain(self.dPOS[Z] * (vel_Z) * (-1), -300, 300)
+        d_z= constrain(self.dPOS[Z] * (vel_Z) * (-1), -500, 500)
 
         self.lastP = [p_x,p_y,p_z]
         self.lastI = [i_x,i_y,i_z]
